739451f4bb95cbefda86b8b4e0d4e1c02e6ae0d1
iOS Blacksmith <ios.blacksmith.info@gmail.com>
2020-10-09 19:06:52 +0200

    Added project

---
+++ Motivator/Motivator/Tools/Colors.swift
@@ -0,0 +1,20 @@
+//
+//  Colors.swift
+//  Motivator
+//
+//  Created by snowman on 09/10/2020.
+//
+
+import UIKit
+
+struct Colors {
+    
+    let sunshine = UIColor(red: 248, green: 186, blue: 0, alpha: 1)
+
+    let mainBlue = UIColor(red: 0, green: 101, blue: 173, alpha: 1)
+    
+    let darkBlue = UIColor(red: 0, green: 57, blue: 97, alpha: 1)
+    
+    let lightBlue = UIColor(red: 0, green: 162, blue: 255, alpha: 1)
+
+}
